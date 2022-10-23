5cb3df857ff69cc1d54cc5bfb50807fed5150edf
hieu.nm <hieu.nm@crossian.com>
2022-10-23 22:56:29 +0700

    herosection example

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,12 @@ import { MainLayout } from '@/component/layout'
 import { useRouter } from 'next/router'
 import { NextPageWithLayout } from '@/models/index'
 import { Box } from '@mui/material'
+import { HeroSection } from '@/component/home'
 
 const Home: NextPageWithLayout = () => {
-  const router = useRouter()
-
-  const goToDetailsPage = () => {
-    router.push({
-      pathname: '/posts/[postId]',
-      query: {
-        postId: '123',
-        ref: 'social'
-      }
-    })
-  }
   return (
     <Box>
-      home Page
+      <HeroSection />
     </Box>
   )
 }
